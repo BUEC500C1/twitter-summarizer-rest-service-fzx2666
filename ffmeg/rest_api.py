e4aca40df31b1f06789f124eddadd8528574abe7
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-03-21 17:32:08 -0300

    Update rest_api.py

--- ffmeg/rest_api.py
+++ ffmeg/rest_api.py
@@ -2,15 +2,24 @@ from flask import Flask, render_template, request, send_file
 from flask_restful import reqparse, Api, Resource
 import os
 import multi_task
-
+import twee
 app = Flask(__name__)
 @app.route('/')
-
 def home_page():
-	multi_task.ThreadHw4()
 	return render_template("dai_twee.html")
 
-def upload():
+@app.route('/', methods = ['POST'])
+def post():
+	d=[]
+	d.append(str(request.form['one']))
+	d.append(str(request.form['two']))
+	d.append(str(request.form['three']))
+	d.append(str(request.form['four']))
+	s = ""
+	for i in d: s = s+" "+i
+	if '' in d: return "Wrong Input"
+	os.system("python3 multi_task.py"+s)
+	print("python3 multi_task.py"+s)
 	return send_file('daily.zip', mimetype='zip',attachment_filename='daily.zip',as_attachment=True)
 
 if __name__ == '__main__':
