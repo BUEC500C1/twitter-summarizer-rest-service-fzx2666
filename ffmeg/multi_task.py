3969554684ef009d7aaac2cc6bd3e853fd91a4cc
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-03-21 17:32:45 -0300

    Update multi_task.py

--- ffmeg/multi_task.py
+++ ffmeg/multi_task.py
@@ -1,31 +1,46 @@
 import twee
-from dataclasses import dataclass, field
+import sys
 import queue
 import threading
 import multiprocessing
+import zipfile
 
 #twee.twitter_data('Nike')
 
+def return_words():
+  return [sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]]
+
 def ThreadHw4():
-  while True:
-    item = q.get()
-    if item is None: break
-    twee.twitter_data(item)
-    q.task_done()
+  def subwork():
+    while True:
+      item = q.get()
+      if item is None: break
+      t = twee.TwitterTimeline()
+      t.get_data(item)
+      q.task_done()
 
-q = queue.Queue()
-threads = []
-for i in range(2):
-  t = threading.Thread(target = ThreadHw4)
-  t.start()
-  threads.append(t)
+  q = queue.Queue()
+  threads = []
+  for i in range(2):
+    t = threading.Thread(target = subwork)
+    t.start()
+    threads.append(t)
 
-for item in ['Nike','Trump','CNN','HBO']:
-  q.put(item)
+  for item in return_words():
+    q.put(item)
 
-q.join()
+  q.join()
 
-for i in range(2):
-  q.put(None)
-for t in threads:
-  t.join()
+  for i in range(2):
+    q.put(None)
+  for t in threads:
+    t.join()
+
+  videos = zipfile.ZipFile('daily.zip','w')
+  for file in return_words():
+    videos.write(file+'-daily twitter.avi')
+    print("zip file has been generated!")
+  return 'A zip file has been uploaded'
+
+if __name__ == "__main__":
+    ThreadHw4()
